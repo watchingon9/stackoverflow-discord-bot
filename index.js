f2119ada528bd1931e18441fb35e241a59ad29c9
Reina Liew <notalk_0@hotmail.com>
2019-06-01 17:17:10 +0800

    Install express

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const http = require('http');
+const express = require('express');
 const discord = require('discord.js');
 const dotenv = require('dotenv');
 const stackexchange = require('stackexchange');
@@ -88,10 +88,6 @@ discord_client.login(process.env.DISCORD_TOKEN);
 /************************
  *  Web Server
  * ********************** */
+const app = express();
 const PORT = process.env.PORT || 5000;
-http
-  .createServer(function(req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-  })
-  .listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log('Server Up and running'));
