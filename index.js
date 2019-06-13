4433b54d8709c19fa2c41a52e07ee9a92fdfa7f7
Reina Liew <notalk_0@hotmail.com>
2019-06-13 22:24:55 +0800

    set activity when boot

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const discord = require('discord.js');
 const dotenv = require('dotenv');
 const stackexchange = require('stackexchange');
 const giphy = require('giphy-js-sdk-core');
-const { _prefix, _q, _filter } = require('./config.json');
+const { _prefix, _filter, _replies } = require('./config.json');
 
 dotenv.config();
 
@@ -25,8 +25,8 @@ var getStackOverflowData = function(keyword, callback) {
   });
 };
 
-var formatReply = function(data) {
-  let combinedString = 'Here are some of the top results: \n\n';
+var formatReply = function(data, userId) {
+  let combinedString = `Dear my boss <@${userId}>, here are some of the top results for you:\n\n`;
   for (let i = 0; i < data.length; i++) {
     combinedString += `Q. ${data[i].title}\nTags: ${data[i].tags.join(
       ' | '
@@ -39,9 +39,9 @@ var formatReply = function(data) {
  *  Giphy
  * ********************** */
 const giphy_client = giphy(process.env.GIPHY_KEY);
-const getGif = function(callback) {
+const getGif = function(tag, callback) {
   giphy_client
-    .search('gifs', { q: _q })
+    .search('gifs', { q: tag })
     .then(response => {
       var totalResponses = response.data.length;
       var responseIndex = Math.floor(Math.random() * 10 + 1) % totalResponses;
@@ -55,27 +55,28 @@ const getGif = function(callback) {
 /************************
  *  Discord
  * ********************** */
-const discord_client = new discord.Client();
+const bot = new discord.Client();
 
-discord_client.once('ready', () => {
-  console.log('Ready!');
+bot.once('ready', () => {
+  bot.user.setActivity('You', { type: 'WATCHING' });
 });
 
-discord_client.on('message', message => {
+bot.on('message', message => {
   const args = message.content.split(' ');
   if (args) {
     if (args[0] === `${_prefix}?`) {
       message.channel.send(
-        `Ask me everything about programming\nCommand: !s [language] [specific questions]`
+        `Ask me everything about programming!\nMy command is: !s [language] [specific questions]`
       );
     } else if (args[0] === `${_prefix}`) {
-      let keywords = message.content.replace(_prefix, '');
+      const keywords = message.content.replace(_prefix, '');
       getStackOverflowData(keywords, results => {
         if (results.length > 0) {
-          message.channel.send(formatReply(results));
+          message.channel.send(formatReply(results, message.author.id));
         } else {
-          getGif(gif => {
-            message.channel.send('um... i think is time to go home.', {
+          const reply = _replies[Math.floor(Math.random() * _replies.length)];
+          getGif(reply.giphy, gif => {
+            message.channel.send(reply.message, {
               files: [gif]
             });
           });
@@ -85,4 +86,4 @@ discord_client.on('message', message => {
   }
 });
 
-discord_client.login(process.env.DISCORD_TOKEN);
+bot.login(process.env.DISCORD_TOKEN);
